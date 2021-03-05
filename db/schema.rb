b3da9234ece0c6c61bc841f87767d1d363e95482
victor holl <v_h@me.com>
2021-03-05 15:44:40 +0100

    add date to commits

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_05_142818) do
+ActiveRecord::Schema.define(version: 2021_03_05_144354) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "commits", force: :cascade do |t|
+    t.text "message"
+    t.boolean "published"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.date "message_date"
+    t.index ["user_id"], name: "index_commits_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -31,4 +41,16 @@ ActiveRecord::Schema.define(version: 2021_03_05_142818) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "votes", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "commit_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["commit_id"], name: "index_votes_on_commit_id"
+    t.index ["user_id"], name: "index_votes_on_user_id"
+  end
+
+  add_foreign_key "commits", "users"
+  add_foreign_key "votes", "commits"
+  add_foreign_key "votes", "users"
 end
