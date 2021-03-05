43e36b6131acfc024fe01e3cbf2133b4a31330bd
Pierre-Yves Le Guennec <pyves.lg@gmail.com>
2021-03-05 15:42:42 +0100

    installer webpacker

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,3 +11,12 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+
+
+import "bootstrap";
+
+document.addEventListener('turbolinks:load', () => {
+  // Call your functions here, e.g:
+
+});
