9d344d7b24f43d35b787dabdf2ae2290caa5acd8
victor holl <v_h@me.com>
2021-03-05 16:55:55 +0100

    add create delete votes

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -3,6 +3,15 @@ class VotesController < ApplicationController
   def create
     commit = Commit.find(params[:commit_id])
     Vote.create(commit: commit, user: current_user)
+    redirect_back(fallback_location: root_path)
+    #change to render as json to fetch from JS, maybe with updated total votes on commit
+  end
+
+  def delete
+    commit = Commit.find(params[:commit_id])
+    Vote.find_by(user: current_user, commit: commit).destroy
+    redirect_back(fallback_location: root_path)
+    #change to render as json to fetch from JS, maybe with updated total votes on commit
   end
 
 end
