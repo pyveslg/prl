6db420a25b7febd831021f8faa62b76d05d9424c
Sunny Ripert <sunny@sunfox.org>
2021-03-05 18:53:52 +0100

    No duplicate comments

--- app/jobs/create_commits_job.rb
+++ app/jobs/create_commits_job.rb
@@ -5,6 +5,8 @@ class CreateCommitsJob < ApplicationJob
     login = user.github_username
     GithubApi::Discovery.each_repository(login) do |repo|
       GithubApi::Discovery.each_commit(login, repo["name"]) do |commit|
+        next if Commit.where(github_id: commit["id"]).any?
+
         Commit.create!(
           user: user,
           github_id: commit["id"],
