4f24eb4d3c82e1034251e19fd1ca1822830ce5e2
victor holl <v_h@me.com>
2021-03-05 19:08:05 +0100

    add first 100

--- app/controllers/commits_controller.rb
+++ app/controllers/commits_controller.rb
@@ -2,9 +2,9 @@ class CommitsController < ApplicationController
 
   def index
     if params[:query]
-      @commits = Commit.joins(:user).where(user: {batch: params[:query]}).by_random
+      @commits = Commit.joins(:user).where(user: {batch: params[:query]}).by_random.first(100)
     else
-      @commits = Commit.by_random
+      @commits = Commit.by_random.first(100)
     end
   end
 end
