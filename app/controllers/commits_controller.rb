479e83b160514fe4f3d680dedf0dd0a0f90de713
Sunny Ripert <sunny@sunfox.org>
2021-03-06 14:20:26 +0100

    Filter by batch and user

--- app/controllers/commits_controller.rb
+++ app/controllers/commits_controller.rb
@@ -1,10 +1,26 @@
 class CommitsController < ApplicationController
 
   def index
-    if params[:query]
-      @commits = Commit.joins(:user).where(user: {batch: params[:query]}).by_random.first(100)
-    else
-      @commits = Commit.by_random.first(100)
+    @commits = filtered_commits
+    @batches = User.distinct.pluck(:batch).compact
+    @usernames = User.order(:first_name, :last_name).map do |user|
+      [user.full_name, user.github_username]
     end
+  end
+
+  private
+
+  def filtered_commits
+    commits = Commit.joins(:user).by_random.limit(100)
+
+    if params[:batch].present?
+      commits = commits.where(user: { batch: params[:batch] })
+    end
+
+    if params[:username].present?
+      commits = commits.where(user: { github_username: params[:username] })
+    end
+
+    commits
   end
 end
