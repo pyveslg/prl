8104012ff5365e29d60fdef079d824222c7e6681
Sunny Ripert <sunny@sunfox.org>
2021-03-06 14:35:22 +0100

    Show total number of comments

--- app/controllers/commits_controller.rb
+++ app/controllers/commits_controller.rb
@@ -1,6 +1,8 @@
 class CommitsController < ApplicationController
 
   def index
+    @total_count = Commit.count
+
     if params[:query]
       @commits = Commit.joins(:user).where(user: {batch: params[:query]}).by_random.first(100)
     else
