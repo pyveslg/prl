a26c391db1518ff53f00df8b3a58efb1d947dbf9
Sunny Ripert <sunny@sunfox.org>
2021-03-07 15:30:52 +0100

    Remove n+1 on votes

--- app/controllers/commits_controller.rb
+++ app/controllers/commits_controller.rb
@@ -5,12 +5,13 @@ class CommitsController < ApplicationController
     @batches = User.distinct.pluck(:batch).compact
     @usernames = usernames
     @commits = filtered_commits
+    @session_votes_by_commit = session_votes_by_commit
   end
 
   private
 
   def filtered_commits
-    commits = Commit.joins(:user).order(score: :desc).by_random.limit(100)
+    commits = Commit.includes(:user).order(score: :desc).by_random.limit(100)
 
     if params[:batch].present?
       commits = commits.where(user: { batch: params[:batch] })
@@ -22,10 +23,16 @@ class CommitsController < ApplicationController
 
     commits
   end
-  
+
   def usernames
     User.order(:first_name, :last_name).map do |user|
       [user.full_name, user.github_username]
+    end
+  end
+
+  def session_votes_by_commit
+    Vote.where(session_id: session.id.to_s).to_h do |vote|
+      [vote.commit_id, vote]
     end
   end
 end
--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -4,17 +4,17 @@ class VotesController < ApplicationController
 
     votes = Vote.where(commit: commit, session_id: session.id.to_s)
     votes.destroy_all
-    votes.create!(value: params[:value]) unless params[:value] == "0"
+    vote = votes.create!(value: params[:value]) unless params[:value] == "0"
 
     commit.update_column(:score, commit.votes.sum(:value))
 
     render json: {
-       votes: commit.score,
-       content: render_to_string(
-         partial: "shared/vote",
-         locals: { commit: commit },
-         layout: false,
-       )
+      votes: commit.score,
+      content: render_to_string(
+        partial: "shared/vote",
+        locals: { commit: commit, vote: vote },
+        layout: false,
+      )
      }
   end
 end
