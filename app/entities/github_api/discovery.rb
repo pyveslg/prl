3da962a3fc57dde8401a65485d40077618b67fe7
Sunny Ripert <sunny@sunfox.org>
2021-03-07 14:25:10 +0100

    Create repositories from a fixed list 🛣 (#33)

--- app/entities/github_api/discovery.rb
+++ app/entities/github_api/discovery.rb
@@ -1,15 +1,6 @@
 module GithubApi::Discovery
   module_function
 
-  def each_repository(username, &block)
-    variables = { username: username }
-    data = GithubApi::Client.query(RepositoriesByUserQuery, variables: variables).data.to_h
-    repositories = data.dig("user", "repositories", "edges")
-    repositories.each do |repository|
-      yield repository["node"]
-    end
-  end
-
   def each_commit(username, repository, cursor = nil, &block)
     variables = { username: username, repository: repository, cursor: cursor }
     data = GithubApi::Client.query(CommitsByRepositoryQuery, variables: variables).data.to_h
--- app/models/repository.rb
+++ app/models/repository.rb
@@ -4,6 +4,12 @@ class Repository < ApplicationRecord
   validates :name, presence: true
   validates :github_username, presence: true
 
+  def self.matching_github_url(url)
+    match = %r{https://github.com/(.*?)/(.*)}.match(url)
+    return none unless match
+    where(github_username: match[1], name: match[2])
+  end
+
   def full_name
     "#{github_username}/#{name}"
   end
