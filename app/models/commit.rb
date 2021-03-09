926acf1808a01d45db33962b2b2d6b28760ea1ec
Victor Holl <v_h@me.com>
2021-03-09 18:49:24 +0100

    add upvoted sorting

--- app/models/commit.rb
+++ app/models/commit.rb
@@ -3,11 +3,13 @@ class Commit < ApplicationRecord
   belongs_to :repository
   has_many :votes, dependent: :destroy
 
-  scope :top, -> { order(score: :desc) }
-  scope :hot, -> { joins('LEFT JOIN votes ON commits.id = votes.commit_id').order('votes.created_at') }
-  scope :recent, -> { order(created_at: :desc) }
-  scope :random, -> { }
+  scope :top, ->(*) { order(score: :desc) }
+  scope :hot, ->(*) { joins('LEFT JOIN votes ON commits.id = votes.commit_id').order('votes.created_at') }
+  scope :recent, ->(*) { order(created_at: :desc) }
+  scope :random, ->(*) { }
+
+  scope :upvoted, ->(session_id) { joins(:votes).where('votes.session_id = ?', session_id.to_s)}
 
   # didn't find a method to list scopes so don't forget to update this constant
-  SCOPES = ['random', 'top', 'hot', 'recent']
+  SCOPES = ['random', 'top', 'hot', 'recent', 'upvoted']
 end
