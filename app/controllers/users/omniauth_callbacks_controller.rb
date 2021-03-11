e97fccae9100504ffb84c1aefbafbf9365c02c3a
Victor Holl <v_h@me.com>
2021-03-11 17:43:50 +0100

    not found. Authentication passthru.

---
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -0,0 +1,18 @@
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  def github
+    # user = User.find_for_facebook_oauth(request.env['omniauth.auth'])
+    user = User.from_omniauth(request.env['omniauth.auth'])
+
+    if user.persisted?
+      sign_in_and_redirect user, event: :authentication
+      set_flash_message(:notice, :success, kind: 'Github') if is_navigational_format?
+    else
+      session['devise.github_data'] = request.env['omniauth.auth']
+      redirect_to new_user_registration_url
+    end
+  end
+
+  def failure
+    redirect_to root_path
+  end
+end
