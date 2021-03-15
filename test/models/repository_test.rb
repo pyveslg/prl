72b3d92825b08cb7eb0f57494157797f5792241a
Sunny Ripert <sunny@sunfox.org>
2021-03-15 14:48:14 +0100

    Add vote and repository unit tests 🥽

---
+++ test/models/repository_test.rb
@@ -0,0 +1,16 @@
+require "test_helper"
+
+class RepositoryTest < ActiveSupport::TestCase
+  test "full_name returns both the github handle and the name" do
+    repository = Repository.new(github_username: "pyveslg", name: "prl")
+    assert_equal "pyveslg/prl", repository.full_name
+  end
+
+  test ".matching_github_url finds repositories by splitting the name" do
+    prl = Repository.create!(github_username: "pyveslg", name: "prl")
+    not_prl = Repository.create!(github_username: "sunny", name: "prl")
+
+    url = "https://github.com/pyveslg/prl"
+    assert_equal [prl], Repository.matching_github_url(url).to_a
+  end
+end
