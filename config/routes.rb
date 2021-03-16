5c362b5f86f91aab572e1fdf98d579dca9b48ec1
Victor Holl <v_h@me.com>
2021-03-16 16:52:38 +0100

    remove comment in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users,
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "pages#home"
   get '/profile', to: 'pages#profile'
 
