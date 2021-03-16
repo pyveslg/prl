f82a430217e7780f0d707b7186483cbeef0859ef
Victor Holl <62886862+v-h-z@users.noreply.github.com>
2021-03-16 16:45:40 +0100

    Update app/controllers/users/omniauth_callbacks_controller.rb
    Co-authored-by: Sunny Ripert <sunny@sunfox.org>

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,7 +1,6 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def github
     user = User.from_omniauth(request.env['omniauth.auth'])
-    p request.env['omniauth.auth']
     if user.persisted?
       sign_in_and_redirect user, event: :authentication
       set_flash_message(:notice, :success, kind: 'Github') if is_navigational_format?
