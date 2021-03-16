3dc5eeb5ad0f334ed66973f8215f1e1af8348799
Victor Holl <62886862+v-h-z@users.noreply.github.com>
2021-03-16 16:46:25 +0100

    Update app/models/user.rb
    Co-authored-by: Sunny Ripert <sunny@sunfox.org>

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,6 @@ class User < ApplicationRecord
   end
 
   def self.from_omniauth(auth)
-    # where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
     where(github_username: auth.info.nickname).first_or_create do |user|
       user.email = auth.info.email
       user.uid = auth.uid
