788e456b893157eb9ba7679a32887e7a4d6774a1
Victor Holl <62886862+v-h-z@users.noreply.github.com>
2021-03-16 16:45:13 +0100

    Update app/controllers/users/omniauth_callbacks_controller.rb
    Co-authored-by: Sunny Ripert <sunny@sunfox.org>

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,6 +1,5 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def github
-    # user = User.find_for_facebook_oauth(request.env['omniauth.auth'])
     user = User.from_omniauth(request.env['omniauth.auth'])
     p request.env['omniauth.auth']
     if user.persisted?
