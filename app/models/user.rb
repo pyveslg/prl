93c1682989acf058e02b25c7f5f0b19f322c92ac
Victor Holl <62886862+v-h-z@users.noreply.github.com>
2021-03-16 16:46:34 +0100

    Update app/models/user.rb
    Co-authored-by: Sunny Ripert <sunny@sunfox.org>

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,9 +16,6 @@ class User < ApplicationRecord
       user.uid = auth.uid
       user.provider = auth.provider
       user.password = Devise.friendly_token[0, 20]
-      # If you are using confirmable and the provider(s) you use validate emails,
-      # uncomment the line below to skip the confirmation emails.
-      # user.skip_confirmation!
     end
   end
 end
