3fc7b2403576b82b3958f088d7d62a7cd59254f9
Victor Holl <62886862+v-h-z@users.noreply.github.com>
2021-03-17 10:53:35 +0100

    Update app/controllers/users/omniauth_callbacks_controller.rb
    Co-authored-by: Sunny Ripert <sunny@sunfox.org>

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,7 +1,9 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def github
     user = User.from_omniauth(request.env['omniauth.auth'])
-    redirect_to root_path, alert: "you're not registered on our website" and return if user.nil?
+    if user.nil?
+      return redirect_to(root_path, alert: "You’re not registered on our website.")
+    end
 
     if user.persisted?
       sign_in_and_redirect user, event: :authentication
