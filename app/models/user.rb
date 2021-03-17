8c1dfe8d2c2233358aafb0b05e97af31115ff24a
Victor Holl <62886862+v-h-z@users.noreply.github.com>
2021-03-17 11:03:11 +0100

    GitHub Omniauth 🔐 (#51)
    * add omniauth gem + devise config
    
    * add thingy to get last version of omniauth
    
    * add oauth columns to users
    
    * not found. Authentication passthru.
    
    * first working oauth github signup
    
    * should create a user or add info to existing by github username
    
    * sad sad links just to have them displayed
    
    * without login link
    
    * Update app/controllers/users/omniauth_callbacks_controller.rb
    
    Co-authored-by: Sunny Ripert <sunny@sunfox.org>
    
    * Update app/controllers/users/omniauth_callbacks_controller.rb
    
    Co-authored-by: Sunny Ripert <sunny@sunfox.org>
    
    * Update app/models/user.rb
    
    Co-authored-by: Sunny Ripert <sunny@sunfox.org>
    
    * Update app/models/user.rb
    
    Co-authored-by: Sunny Ripert <sunny@sunfox.org>
    
    * Update app/views/commits/index.html.erb
    
    Co-authored-by: Sunny Ripert <sunny@sunfox.org>
    
    * Update app/views/layouts/application.html.erb
    
    Co-authored-by: Sunny Ripert <sunny@sunfox.org>
    
    * move ngrok host in ENV
    
    * remove comment in routes
    
    * add how to in readme
    
    * test how to avoid create by omniauth
    
    * prevent registering via omniauth. only login ispossible
    
    * Update README.md
    
    Co-authored-by: Sunny Ripert <sunny@sunfox.org>
    
    * Update README.md
    
    Co-authored-by: Sunny Ripert <sunny@sunfox.org>
    
    * Update README.md
    
    Co-authored-by: Sunny Ripert <sunny@sunfox.org>
    
    * Update README.md
    
    Co-authored-by: Sunny Ripert <sunny@sunfox.org>
    
    * Update app/views/layouts/application.html.erb
    
    Co-authored-by: Sunny Ripert <sunny@sunfox.org>
    
    * Update app/views/layouts/application.html.erb
    
    Co-authored-by: Sunny Ripert <sunny@sunfox.org>
    
    * Update README.md
    
    Co-authored-by: Sunny Ripert <sunny@sunfox.org>
    
    * Update app/controllers/users/omniauth_callbacks_controller.rb
    
    Co-authored-by: Sunny Ripert <sunny@sunfox.org>
    
    * Update app/views/pages/profile.html.erb
    
    Co-authored-by: Sunny Ripert <sunny@sunfox.org>
    
    * keep only 551 batch in seed
    
    * Update app/controllers/users/omniauth_callbacks_controller.rb
    
    Co-authored-by: Sunny Ripert <sunny@sunfox.org>
    
    * remove singup and singin forms
    
    Co-authored-by: Sunny Ripert <sunny@sunfox.org>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  # devise :database_authenticatable, :registerable,
+  #        :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :omniauthable, omniauth_providers: [:github]
   has_many :commits, dependent: :destroy
 
   def full_name
@@ -15,5 +16,18 @@ class User < ApplicationRecord
     return if last_name.blank?
 
     "#{last_name[0]}."
+  end
+
+  def self.from_omniauth(auth)
+    user = where(github_username: auth.info.nickname).first
+    return unless user
+
+    user.update(
+      email: auth.info.email,
+      uid: auth.uid,
+      provider: auth.provider,
+      password: Devise.friendly_token[0, 20]
+    )
+    user
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users,
+    controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: "pages#home"
   get :about, to: "pages#about"
+  get '/profile', to: 'pages#profile'
 
   get ":scope",
     to: "commits#index",
