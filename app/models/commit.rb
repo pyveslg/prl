82305654675bb044a40743786798f34dfc650e97
Victor Holl <62886862+v-h-z@users.noreply.github.com>
2021-03-17 10:43:10 +0100

    Add some sorting options 🌶 (#44)
    * add some sorting options
    
    * add upvoted sorting
    
    * sort upvoted by recent
    
    * more like voted than upvoted amirite?
    
    * all instead of nothing
    
    * secure path by changing SCOPES to hash
    
    * send -> public_send
    
    * constant issues with packages
    
    * params + sort
    
    * forgot a console
    
    * Update app/models/commit.rb
    
    Co-authored-by: Sunny Ripert <sunny@sunfox.org>
    
    * move random to scopes
    
    * path -> params[:scope]
    
    * Update app/controllers/commits_controller.rb
    
    Co-authored-by: Sunny Ripert <sunny@sunfox.org>
    
    * Update app/models/commit.rb
    
    Co-authored-by: Sunny Ripert <sunny@sunfox.org>
    
    * Update app/models/commit.rb
    
    Co-authored-by: Sunny Ripert <sunny@sunfox.org>
    
    * change values for keys
    
    * found a way for constraints and add selected if selected on index
    
    * Update app/views/commits/index.html.erb
    
    Co-authored-by: Sunny Ripert <sunny@sunfox.org>
    
    Co-authored-by: Sunny Ripert <sunny@sunfox.org>

--- app/models/commit.rb
+++ app/models/commit.rb
@@ -2,4 +2,22 @@ class Commit < ApplicationRecord
   belongs_to :user
   belongs_to :repository
   has_many :votes, dependent: :destroy
+
+  scope :top, ->(*) { order(score: :desc) }
+  scope :hot, ->(*) { left_joins(:votes).order(Vote.arel_table[:created_at]) }
+  scope :recent, ->(*) { order(created_at: :desc) }
+  scope :random, ->(*) { by_random }
+
+  scope :voted, ->(session_id) {
+    joins(:votes).where(votes: { session_id: session_id.to_s }).hot
+  }
+
+  # Scopes that are publicly available (path => scope name)
+  SCOPES = {
+    'random' => 'random',
+    'top' => 'top',
+    'hot' => 'hot',
+    'recent' => 'recent',
+    'voted' => 'voted'
+  }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   root to: "pages#home"
   get :about, to: "pages#about"
-  resources :commits, only: :index
+
+  get ":scope",
+    to: "commits#index",
+    constraints: lambda {|request| Commit::SCOPES.keys.include?(request[:scope])},
+    as: :scopes
+
   resources :votes, only: :create
 end
