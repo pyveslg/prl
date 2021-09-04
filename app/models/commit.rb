35cf1bd75a7341ed868dcb466fa1d7288b710f73
Sunny Ripert <sunny@sunfox.org>
2021-09-04 12:49:11 +0200

    Reorder scopes

--- app/models/commit.rb
+++ app/models/commit.rb
@@ -3,15 +3,6 @@ class Commit < ApplicationRecord
   belongs_to :repository
   has_many :votes, dependent: :destroy
 
-  scope :top, ->(*) { order(score: :desc) }
-  scope :hot, ->(*) { left_joins(:votes).order(Vote.arel_table[:created_at]) }
-  scope :recent, ->(*) { order(created_at: :desc) }
-  scope :random, ->(*) { by_random }
-
-  scope :voted, ->(session_id) {
-    joins(:votes).where(votes: { session_id: session_id.to_s }).hot
-  }
-
   # Scopes that are publicly available (path => scope name)
   SCOPES = %w[
     top
@@ -20,4 +11,14 @@ class Commit < ApplicationRecord
     recent
     voted
   ]
+
+  scope :top, ->(_session_id) { order(score: :desc) }
+  scope :random, ->(_session_id) { by_random }
+  scope :hot, ->(_session_id) {
+    left_joins(:votes).order(Vote.arel_table[:created_at])
+  }
+  scope :recent, ->(_session_id) { order(created_at: :desc) }
+  scope :voted, ->(session_id) {
+    joins(:votes).where(votes: { session_id: session_id.to_s }).hot
+  }
 end
