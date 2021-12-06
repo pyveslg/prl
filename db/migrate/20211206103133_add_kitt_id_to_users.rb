0760d14649337a29b6e98d355b2dc7e0a249da19
Thibaud Maurel <maurel.thibaud@free.fr>
2021-12-06 11:43:54 +0100

    remove useless comment

--- db/migrate/20211206103133_add_kitt_id_to_users.rb
+++ db/migrate/20211206103133_add_kitt_id_to_users.rb
@@ -19,6 +19,3 @@ class AddKittIdToUsers < ActiveRecord::Migration[6.1]
     end
   end
 end
-
-#{"alumnus"=>{"id"=>12267, "first_name"=>"Pierre", "last_name"=>"BRIDE", "current_experience"=>{"picture"=>{"url"=>"//d26jy9fbi4q9wx.cloudfront.net/assets/company_placeholder-c8e19ec0f6ab43538d9b82ce6e897c681f2acf389f1f26a2c4646052d9968ee2.png", "avatar"=>{"url"=>"//d26jy9fbi4q9wx.cloudfront.net/assets/company_placeholder-c8e19ec0f6ab43538d9b82ce6e897c681f2acf389f1f26a2c4646052d9968ee2.png"}}, "url"=>nil, "position"=>"Locataire Gérant  at Carrefour City Évreux Jaurès"}, "avatar"=>"https://res.cloudinary.com/wagon/image/upload/c_fill,g_face,h_200,w_200/v1625326723/liyji6t4euy6jtddjguz.jpg", "github"=>"pitbride", "alumni"=>true, "camp_slug"=>"660", "camp_date"=>"Jul '21", "city"=>"Paris", "engineering"=>false, "core"=>false, "teacher"=>true, "teaching_role"=>"TA", "since"=>"Oct '21"}}
-#Pierre BRIDE created
