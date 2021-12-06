98ce5f74fed4df030a58e09a566d779a3a5a557a
Sunny Ripert <sunny@sunfox.org>
2021-12-06 11:42:47 +0100

    Fix GitHub API env name and prefer symbols 🧖‍♂️

--- app/jobs/create_commits_job.rb
+++ app/jobs/create_commits_job.rb
@@ -4,23 +4,23 @@ class CreateCommitsJob < ApplicationJob
   def perform(repository)
     api = Github::Api.new
     api.each_commit(repository.github_username, repository.name) do |commit|
-      next if commit.dig("committer", "email") == "noreply@github.com"
+      next if commit.dig(:committer, :email) == "noreply@github.com"
 
-      hash = commit["id"]
+      hash = commit[:id]
       next if Commit.where(github_id: hash).any?
 
-      author_login = commit.dig("author", "user", "login")
+      author_login = commit.dig(:author, :user, :login)
       author = User.find_by(github_username: author_login)
       next if author.nil?
 
-      message = commit["message"].lines.first.strip
+      message = commit[:message].lines.first.strip
 
       puts "#{repository.full_name}: <#{author_login}> #{message}"
       Commit.create!(
         user: author,
         github_id: hash,
-        message: commit["message"].lines.first,
-        message_date: commit["committedDate"],
+        message: commit[:message].lines.first,
+        message_date: commit[:committedDate],
         repository: repository,
       )
     end
