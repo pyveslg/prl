37533c6044b5305935b3cb9fabde71c0c2423805
Thibaud Maurel <30435844+thmaurel@users.noreply.github.com>
2022-02-02 21:24:58 +0100

    Update app/jobs/clean_old_commits_job.rb
    Co-authored-by: Sunny Ripert <sunny@sunfox.org>

--- app/jobs/clean_old_commits_job.rb
+++ app/jobs/clean_old_commits_job.rb
@@ -2,6 +2,6 @@ class CleanOldCommitsJob < ApplicationJob
   queue_as :default
 
   def perform
-    Commit.where("created_at < '#{Time.now - 4.month}' ").where('score <= 0').destroy_all
+    Commit.where(created_at: ..4.months.ago).where(score: ..0).destroy_all
   end
 end
